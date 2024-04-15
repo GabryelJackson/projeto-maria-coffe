7998c5a0115b68baaa378e7e798f2f05ecd3a655
GabryelJackson <gabryeljackson5@gmail.com>
2024-04-14 21:37:43 -0300

    site criado

---
+++ script.js
@@ -0,0 +1,7 @@
+// Adicionando funcionalidade ao formulário de contato
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    // Simulando o envio do formulário
+    alert('Mensagem enviada com sucesso!');
+   
+});
